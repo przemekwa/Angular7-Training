a54fde02082a9d831126d3188fa66cd4651ff944
Przemyslaw Walkowski <przemyslaw.walkowski@snpgroup.com>
2019-03-07 15:45:17 +0100

    updates

--- src/app/space/decoder-waves/decoder-waves.component.ts
+++ src/app/space/decoder-waves/decoder-waves.component.ts
@@ -1,6 +1,5 @@
-import * as mastermind from 'numeric-mastermind-solver-js';
-import { Component, OnInit } from '@angular/core';
-
+import * as mastermind from "numeric-mastermind-solver-js";
+import { Component, OnInit } from "@angular/core";
 
 export interface INumber {
   numbers: string;
@@ -8,46 +7,34 @@ export interface INumber {
 
 export interface IResult {
   number: string;
-  pins:string[]
+  pins: string[];
 }
 
-
 @Component({
-  selector: 'app-decoder-waves',
-  templateUrl: './decoder-waves.component.html',
-  styleUrls: ['./decoder-waves.component.css']
+  selector: "app-decoder-waves",
+  templateUrl: "./decoder-waves.component.html",
+  styleUrls: ["./decoder-waves.component.css"]
 })
 export class DecoderWavesComponent implements OnInit {
+  resultList?: IResult[] = [];
+  solution: string = "3235";
 
-  resultList?:IResult[] = [];
-  solution :string = "3235";
-
-  constructor() { }
-
-
+  constructor() {}
 
   checkNumber(formValues: INumber) {
     let res = mastermind.matchPins(this.solution, formValues.numbers);
 
-    let res2:any[] = [];
+    let res2: any[] = [];
 
     for (var _i = 0; _i < res.green; _i++) {
-
       res2.push("G");
+    }
+
+    for (var _i = 0; _i < res.blue; _i++) {
+      res2.push("B");
+    }
+
+    this.resultList.push({ number: formValues.numbers, pins: res2 });
   }
-
-  for (var _i = 0; _i < res.blue; _i++) {
-
-    res2.push("B");
-}
-
-
-
-    this.resultList.push({number:formValues.numbers, pins: res2 });
-  }
-  ngOnInit() {
-
-
-  }
-
+  ngOnInit() {}
 }
