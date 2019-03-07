fc6539b25d08d84d0d42726b81da5f1147e27970
Przemyslaw Walkowski <przemyslaw.walkowski@snpgroup.com>
2019-03-07 09:11:54 +0100

    update planet

--- src/app/space/planet-detector/planet-detector.component.ts
+++ src/app/space/planet-detector/planet-detector.component.ts
@@ -1,39 +1,69 @@
 import { Component, OnInit } from '@angular/core';
 
+interface IPosition {
+  X:number,
+  Y:number
+}
+
 @Component({
   selector: 'app-planet-detector',
   templateUrl: './planet-detector.component.html',
   styleUrls: ['./planet-detector.component.css']
 })
-export class PlanetDetectorComponent implements OnInit {
+export class PlanetDetectorComponent {
 
   imageUrl: string = "assets/noice.jpg"
   x:number = 185;
   y:number = 164;
-
-
   distance = 0.9;
 
-  constructor() { }
+  private context: any;
 
-  update(position:any) {
-    //console.log("x: " + position.x + " y: " + position.y);
-   // console.log(this.IsClose(position));
+  constructor() {
 
-    const max=225;
 
-  this.distance = 1-((max/this.IsClose(position))/10);
-  console.log(this.distance);
+  }
+  draw(position:IPosition) {
+    const canvas = <HTMLCanvasElement>document.getElementById('test');
+
+    this.context = canvas.getContext('2d');
+    this.context.clearRect(0, 0, canvas.width, canvas.height);
+
+    var img = new Image();
+    img.src=this.imageUrl;
+    this.context.drawImage(img, 10, 10);
+
+
+    this.context.beginPath();
+    this.context.strokeStyle = "red"
+    this.context.lineCap = "round";
+    this.context.moveTo(0, position.Y);
+    this.context.lineTo(300, position.Y);
+    this.context.stroke();
+
+    this.context.beginPath();
+    this.context.strokeStyle = "red"
+    this.context.lineCap = "round";
+    this.context.moveTo(position.X, 0);
+    this.context.lineTo(position.X, 300);
+    this.context.stroke();
+
+
+
+    window.requestAnimationFrame(() => this.draw(position));
+
+
   }
 
-  IsClose(currentPosition:any){
-    const dx = currentPosition.x - this.x;
-    const dy = currentPosition.y - this.y;
+  update(position:IPosition) {
+    const max:number=225;
+    this.distance = 1-((max/this.IsClose(position))/10);
+    this.draw(position);
+  }
+
+  IsClose(currentPosition:IPosition){
+    const dx = currentPosition.X - this.x;
+    const dy = currentPosition.Y - this.y;
     return Math.sqrt(dx*dx + dy*dy);
   }
-
-
-  ngOnInit() {
-  }
-
 }
